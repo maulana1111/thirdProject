a04d8a6014def8dab77eaec31f7be4f3962f67b1
mdevinfr <devinfrr@gmail.com>
2022-12-13 18:29:18 +0700

    menghilangkan swagger

--- src/main/java/com/example/thirdproject/controller/WelcomeController.java
+++ src/main/java/com/example/thirdproject/controller/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.example.thirdproject.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class WelcomeController {
+    @Autowired(required = true)
     @GetMapping("/")
     public String welcome() {
         return "Welcome";
